43e7991b3ea5907bf1c4dd732cbe1b7f50621c53
Paragon <hamidouaze@gmail.com>
2025-04-07 15:40:15 +0100

    refactor: update styling of App component and Card to enhance visual appearance

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import './App.css'
 function App() {
   
   return (
-    <div className="flex flex-col gap-4 items-center justify-center h-screen bg-gray-100">
-      <h1 className='text-2xl font-bold'>React Router Dom </h1>
+    <div className="flex flex-col gap-4 items-center justify-center h-screen bg-neutral-300">
+      <h1 className='text-3xl font-bold shadow-violet-700 text-blue-500'>React Router Dom </h1>
       
       <Card name="Avatar" />
       <Card name="Avenger" />
@@ -33,12 +33,12 @@ const Card = ({name}) => {
   console.log(hasLiked)
 
   return (
-    <div className="border-2 border-gray-300 rounded-lg p-4 bg-white shadow-md w-1/4">
+    <div className="border-2 border-gray-300 rounded-lg p-4 bg-blue-400 shadow-md w-1/4">
       <div className='flex gap-2 justify-between'>
         <h2 className='font-medium text-lg items-center m-2'>{name} {hasLiked ? '❤️' : '👎'}</h2>
         <button className='border-1 border-black m-2 rounded-lg p-2 bg-gray-200 w-25 hover:bg-gray-300 cursor-pointer' onClick={handleLikeClick}>{hasLiked ? 'Liked' : 'Like'}</button>
       </div>
-      <p className='text-sm'>Number of Likes: {clickNumber}</p>
+      <p className='text-sm text-amber-50'>Number of Likes: {clickNumber}</p>
 
     </div>
   )
