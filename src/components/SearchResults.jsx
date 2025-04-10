cfbb3f938a0b727003dd76a892bb3b6b95ec2303
Paragon <hamidouaze@gmail.com>
2025-04-10 23:14:34 +0100

    fix: update search prop in SearchResults component for consistency

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,5 +1,5 @@
 
-const SearchResults = ({ searchedMovies, searching, isSearchClicked, searchResultsRef }) => {
+const SearchResults = ({ searchedMovies, searchTerm, isSearchClicked, searchResultsRef }) => {
     if (!isSearchClicked) {
         // If isSearchClicked is false, do not render the component
         return null
@@ -10,14 +10,14 @@ const SearchResults = ({ searchedMovies, searching, isSearchClicked, searchResul
         <div className='searched Movie' ref={searchResultsRef}>
         {/* Title showing the current search query if present */}
             <h2 className='mb-5'>
-              {searching ? 'Search for : ' : ''} {searching}
+              {searchTerm ? 'Search for : ' : ''} {searchTerm}
             </h2>
             {/* Grid layout for search result movies */}
             <ul className='grid grid-cols-6 gap-4'>
                 {/* Loop through and render up to the first 12 search result movies */}
                 {searchedMovies.slice(0, 12).map((movie) => (
                 <li className='relative shadow-md cursor-pointer transition-transform duration-300 ease-in-out transform hover:scale-105' key={movie.id}>
-                {/*isSearchClicked ? (*/}
+                {searchTerm ? (
                     <div className='rounded-md'>
                     {/* Movie poster image with rounded corners */}
                         <img className='rounded-md' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
@@ -26,12 +26,15 @@ const SearchResults = ({ searchedMovies, searching, isSearchClicked, searchResul
                             {/* Movie title displayed at the center */}
                             <h3 className='text-center font-bold text-lg my-4'>{movie.title}</h3>
                             {/* Movie overview text */}
-                            <p className='text-sm overflow-hidden text-clip rtl:text-start' dir="rtl">
-                            Overview: {movie.overview.length > 130 ? movie.overview.slice(0, 130) + '...' : movie.overview}
+                            <p className='text-sm overflow-hidden text-clip'>
+                            Overview: {movie.overview.length > 100 ? movie.overview.slice(0, 100) + '...' : movie.overview}
                             </p>
+                            <br />
+                            {/* Movie rating */}
+                            <p className='text-sm float-end'>Rating: {Math.floor(movie.vote_average*10)}%</p>
+                        </div>
                     </div>
-                  </div>
-                {/*) : (<></>)*/}
+                ) : (null)}
                 </li>
                 ))}
             </ul>
