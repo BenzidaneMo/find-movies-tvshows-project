c2a1e2bf2c09b6c512116c7cdabfcd13daea2556
Paragon <hamidouaze@gmail.com>
2025-04-12 14:33:18 +0100

    fix: restructure Header component layout for improved organization and update CSS styles for consistency

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,16 @@ import Search from './Search'
 const Header = ({ handleSearching, searchTerm, handleSearchIconClick, searchRef }) => {
   
     return (
-        <header className='mb-30'>
-          {/* Logo acts as a link to the home page */}
-          <a href="/">
-            <img src="logo.png" alt="logo" className='w-25 mb-10 cursor-pointer' />
-          </a>
+        <header className='mb-30 mt-0'>
+          <div className='w-full flex flex-col items-center'>
+            {/* Logo acts as a link to the home page */}
+            <a href="/" className='mb-10'>
+              {/* Logo image */}
+              <img src="logo.png" alt="logo" className='min-md:w-25 w-20 h-auto self-center cursor-pointer' />
+            </a>
           {/* Hero image displayed below the logo */}
           <img src="hero.png" alt="img displaying movies" />
+          </div>
           {/* Main title with highlighted text */}
           <h1>
             Find <span className='text-gradient'>Movies</span> You&apos;ll Enjoy Without the Hassle
