268754ace4921351b87d9b42b5552cb7cee4de47
Paragon <hamidouaze@gmail.com>
2025-04-18 21:53:00 +0100

    feat: add Appwrite dependency and implement updateSearchCount function

---
+++ src/appwrite.js
@@ -0,0 +1,9 @@
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+
+
+export const updateSearchCount = async (searchTerm, movie) => {
+    console.log('Database ID:', DATABASE_ID);
+    
+}
