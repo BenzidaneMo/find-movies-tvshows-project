4aa4ff910a76561430113cb96a5245ff3e71837e
Paragon <hamidouaze@gmail.com>
2025-04-21 23:29:55 +0100

    fix: refactor the layout structure in Search component so that when the user click outside the search component the search result doesn't directly disapear

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,22 +8,24 @@ const Search = ({ handleSearching, searchTerm, handleSearchIconClick, searchRef 
   };
 
   return (
-    <div className='search' ref={searchRef}> {/* Attach the ref to the main div */}
-      <div>
-        <img
-          src="search.svg"
-          alt="search icon"
-          className='relative cursor-pointer transition-transform duration-200 ease-in-out transform hover:scale-110 active:scale-90'
-          onClick={handleSearchIconClick} // Call the function when the image is clicked
-        />
-        <input
-          type="text"
-          name="search-for-movie"
-          onChange={handleSearching}
-          value={searchTerm}
-          placeholder='Search through 300+ movies online'
-          onKeyDown={handleKeyDown} // Call handleKeyDown on key press
-        />
+    <div className="w-full bg-transparent h-40" ref={searchRef}> {/* Attach the ref to the main div */}
+      <div className='search'>
+        <div>
+          <img
+            src="search.svg"
+            alt="search icon"
+            className='cursor-pointer transition-transform duration-200 ease-in-out transform hover:scale-110 active:scale-90'
+            onClick={handleSearchIconClick} // Call the function when the image is clicked
+          />
+          <input
+            type="text"
+            name="search-for-movie"
+            onChange={handleSearching}
+            value={searchTerm}
+            placeholder='Search through 300+ movies online'
+            onKeyDown={handleKeyDown} // Call handleKeyDown on key press
+          />
+        </div>
       </div>
     </div>
   );
